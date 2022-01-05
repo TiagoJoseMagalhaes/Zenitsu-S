92da862ca1daef81533f8dfa414d046e84d6984c
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2022-01-05 23:56:33 +0000

    initial commit

---
+++ PentaPsymo/PentaPsymo.cpp
@@ -0,0 +1,43 @@
+﻿// PentaPsymo.cpp : Defines the entry point for the application.
+//
+
+#include "PentaPsymo.h"
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+
+struct RAX
+{
+	union
+	{
+		uint64 reg;
+		struct EAX
+		{
+			union {
+				uint32 reg;
+				struct AX
+				{
+					union
+					{
+						uint16 reg;
+						struct base
+						{
+							uint8 al;
+							uint8 ah;
+						} base;
+					} AX;
+					uint16 pad;
+				} AX;
+			} EAX;
+			uint32 pad;
+		} EAX;
+	} RAX;
+};
+
+int main()
+{
+	RAX reg;
+	return 0;
+}
